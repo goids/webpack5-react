0e278d00b98e19fa582bfa7f0188f836121b1ca4
Diego F. Prado <goidesdesign@gmail.com>
2021-05-26 16:16:23 -0500

    Add Plugins for optimization, Sass and Loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/',
     },
     mode: 'production',
     resolve: {
-        extensions: ['.js','.jsx']
+        extensions: ['.js','.jsx'],
+        alias: {
+            '@components': path.resolve(__dirname, 'src/components/'),
+            '@styles': path.resolve(__dirname, 'src/styles/')
+        }
     }, 
     module:{
         rules: [
@@ -27,6 +36,14 @@ module.exports = {
                         loader: 'html-loader'
                     }
                 ]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
     },
@@ -35,12 +52,16 @@ module.exports = {
             template: './public/index.html',
             filename: './index.html',
         }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+        new CleanWebpackPlugin(),
     ],
-    // optimization: {}
-    devServer:{
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 3006,
-        open: "Brave Browser",
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin()
+        ]
     }
 }
